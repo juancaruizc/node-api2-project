76ea9d1d2d944ab9d26abdfc86a40e12fedbb421
juancaruizc <juancruizcruz@gmail.com>
2021-02-09 15:11:33 -0700

    initial commit

--- api/server.js
+++ api/server.js
@@ -1,2 +1,13 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('Hello from Mars');
+});
+
+module.exports = server;
